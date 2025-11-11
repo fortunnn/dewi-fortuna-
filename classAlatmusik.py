d608a3b67da8cd295e990756ad56bc045a10dc68
fortunnn <dewifortunabaliba9@gmail.com>
2025-11-11 13:09:21 +0700

    Add files via upload

---
+++ classAlatmusik.py
@@ -0,0 +1,31 @@
+class AlatMusik:
+    nama1 = "suling"
+    asal1 = "jawa barat"
+    bahan1 = "bambu"
+    caradimainkan1 = "di tiup"
+    jenis1="tradisional"
+    
+    nama2 = "gendang"
+    asal2 = "jawa barat"
+    bahan2 = "kayu dan kulit sapi"
+    caradimainkan2 = "Dipukul"
+    jenis2= "tradisional"
+    
+alat1 = AlatMusik()
+alat2 = AlatMusik()
+
+print("Alat 1:")
+print(alat1.nama1)
+print(alat1.asal1)
+print(alat1.bahan1)
+print(alat1.caradimainkan1)
+print(alat1.jenis1)
+print()
+
+print("Alat 2:")
+print(alat2.nama2)
+print(alat2.asal2)
+print(alat2.bahan2)
+print(alat2.caradimainkan2)
+print(alat2.jenis)
+print()
